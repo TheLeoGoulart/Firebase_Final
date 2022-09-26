993edccc19f3350850814621e4219f3f13daf8a5
TheLeoGoulart <Leonardo.p.goulart4@gmail.com>
2022-09-25 21:01:33 -0300

    Instalação Firebase

--- projeto-final/src/app/app.module.ts
+++ projeto-final/src/app/app.module.ts
@@ -9,6 +9,13 @@ import { DashboardModule } from './dashboard';
 import { CalculadoraModule } from './calculadora';
 import { TarefasModule } from './tarefas';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { environment } from '../environments/environment';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -21,6 +28,11 @@ import { TarefasModule } from './tarefas';
     CalculadoraModule,
     TarefasModule,
     AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireDatabaseModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- projeto-final/src/environments/environment.ts
+++ projeto-final/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCjV9RweqAr9owZCd0wvzVT6uK4D58xAw4",
+    authDomain: "testefirebase-17bfe.firebaseapp.com",
+    projectId: "testefirebase-17bfe",
+    storageBucket: "testefirebase-17bfe.appspot.com",
+    messagingSenderId: "117608335460",
+    appId: "1:117608335460:web:62128ebb04dd008c61b771",
+    measurementId: "G-DJ4LQ5ZRJ8"
+  },
 };
 
 /*
