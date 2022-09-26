c496e8aef714c68b816030a776b3beee9facf0a4
TheLeoGoulart <Leonardo.p.goulart4@gmail.com>
2022-09-25 21:40:04 -0300

    Criação de Sign In/Up

--- projeto-final/src/app/sign-in/sign-in.component.ts
+++ projeto-final/src/app/sign-in/sign-in.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from ".././shared";
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.css']
+  styleUrls: ['./sign-in.component.scss']
 })
+
 export class SignInComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor(
+    public authService: AuthService
+  ) { }
+  ngOnInit() { }
 }
